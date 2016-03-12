cb9be56998127426e84ab442fb0e5dd298e4131c
linzhongming <lin.zh.ming.27@gmail.com>
2016-03-13 01:47:26 +0800

    no message

--- js/bannerChange.js
+++ js/bannerChange.js
@@ -1,6 +1,14 @@
 /**
  * Created by linzhongming on 16/3/12.
  */
+function TestJS(){
+    //测试操作dom样式
+    var testJS=document.getElementById("searchBtn");
+    testJS.style.color="black";
+}
 
-var sheet=document.styleSheets[2];
-var rules=sheet.cssRules||sheet.rules;
+function  bannerChange(buttonValue) {
+    var changeBannerBoard = document.getElementById("imgBox");
+    var topMovePx=(buttonValue-1)*(-315);
+    changeBannerBoard.style.top = topMovePx;
+}
