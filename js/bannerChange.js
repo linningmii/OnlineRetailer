315a8dbfd21473f189f996a522209110b38f5576
linzhongming <lin.zh.ming.27@gmail.com>
2016-03-14 01:48:37 +0800

    no message

--- js/bannerChange.js
+++ js/bannerChange.js
@@ -18,9 +18,12 @@ function TestJS(x) {
 }
 
 function bannerChange(buttonValue) {
+    //切换图片内容
     var changeBannerBoard = document.getElementById("imgBox");
-    var topMovePx = (buttonValue - 1) * (-315);
+    var topMovePx = (buttonValue - 1) * (-315);    //图片纵向排列,默认高度为315px,初始top为0,top每减少315px即切换到下一张图片
     changeBannerBoard.style.top = topMovePx + "px";
+
+    //切换数字按钮样式
     var bannerPageNumber = document.getElementsByName("bannerPageNumber");
     for (var btnNum = 1; btnNum < 6; btnNum++) {
         bannerPageNumber[btnNum - 1].className = "inactiveBannerBoard";
